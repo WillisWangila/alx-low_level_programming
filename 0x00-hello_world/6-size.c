0b6fc422a694ce547d0d5b911a6d297debdd60d7
Willis Wangila <w.wangila@gmail.com>
2022-10-15 01:37:08 +0000

    Added 6-size.c for Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program printing text with printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %x byte(s)\n", sizeof(char));
+	printf("Size of an int: %x byte(s)\n", sizeof(int));
+	printf("Size of a long int: %x byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %x byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %x byte(s)\n", sizeof(float));
+	return (0);
+}

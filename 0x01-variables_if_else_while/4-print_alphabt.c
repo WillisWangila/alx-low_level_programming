ae211590f4a2c2e104dd9aeb17c5f2be7a71dc06
Willis Wangila <w.wangila@gmail.com>
2022-10-16 22:57:14 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all lowercase characters except 'q' and 'e'
+ * Return: 0
+*/
+
+int main(void)
+{
+	char lc_alphabet;
+
+	lc_alphabet = 'a';
+	while (lc_alphabet <= 'z')
+	{
+		if ((lc_alphabet != 'q') && (lc_alphabet!= 'e'))
+		{
+			putchar(lc_alphabet);
+		}
+		lc_alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}

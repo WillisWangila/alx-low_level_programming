2b9ab3680be2605045f67ef448f0b09f4ef876b3
Willis Wangila <w.wangila@gmail.com>
2022-10-19 02:39:15 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the sum
+ * @a: first integer
+ * @b: second integer
+ * Return: returns the sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}

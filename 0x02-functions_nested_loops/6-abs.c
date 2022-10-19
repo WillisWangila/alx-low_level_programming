54f7401214e95679684cc1256853cc6f2994b127
Willis Wangila <w.wangila@gmail.com>
2022-10-19 01:04:10 +0000

    Initial commit Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @x: integer to be checked
+ * Return: Absolute
+*/
+
+int _abs(int x)
+{
+	if (x > 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}

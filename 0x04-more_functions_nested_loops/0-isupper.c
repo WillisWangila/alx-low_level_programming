489b408a4cc2bf931fbd0ee989a565524531a0f2
Willis Wangila <w.wangila@gmail.com>
2022-10-21 23:48:11 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - checks for uppercase character
+ * _isupper - checks for uppercase character
  * @c: Character to check
  * Return: 1 if uppercase character and 0 if otherwise
 */
 
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
+	if ((c > 64) && (c < 91))
 	{
 		return (1);
 	}

ae44207d38c3f29ea0407652d5efeb8962cbe3df
Willis Wangila <w.wangila@gmail.com>
2022-10-21 23:57:44 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of the 2 integers
+*/
+
+int mul(int a, int b)
+{
+	int product = a * b;
+	return (product);
+}

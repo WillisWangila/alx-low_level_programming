d0f12d2f99e47f2d61ac93b35fabd9c4819229d3
Willis Wangila <w.wangila@gmail.com>
2022-10-25 22:43:43 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - finds length of string
+ * @s: string to be evaluated
+ * Return: count
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		count++;
+	}
+	return (count);
+}

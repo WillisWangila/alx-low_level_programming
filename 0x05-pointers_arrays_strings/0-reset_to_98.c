6c603152f14bc3a1c6cc03637e5f5afff2904793
Willis Wangila <w.wangila@gmail.com>
2022-10-25 22:22:33 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of an int to 98 via its pointer
+ * @n: pointer to int to be updated
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

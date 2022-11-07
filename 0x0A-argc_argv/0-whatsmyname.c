6b293dbb445c5213503af3e80e0e2fcb51a24384
Willis Wangila <w.wangila@gmail.com>
2022-11-07 19:57:12 +0000

    Initial commit Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints program name followed by new line
+ * @argc: holds number of arguments passed to the command line
+ * @argv: holds pointers to addresses of strings passed as arguments
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv[0]);
+	return (0);
+}
